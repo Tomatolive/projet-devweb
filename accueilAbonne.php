7a9326aabe36d5705b186082afe3fcbe8b55f4c1
cheaurizar <tamon.o13@gmail.com>
2024-03-26 16:52:24 +0100

    modified:   accueilAbonne.php 	new file:   css/acceuilAbonne.css

--- accueilAbonne.php
+++ accueilAbonne.php
@@ -31,11 +31,42 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Accueil</title>
+        <meta charset="utf-8">
+        <title>Page de connexion</title>
+        <link rel="stylesheet" href="css/acceuilAbonne.css">
     </head>
     <body>
-        <div class="titre">
-          <h1 id="accueil">Accueil abonne</h1>
+        <header>
+            <a href="index.html">
+                <img src="css/img/logo.png" alt="Logo du site" id="LogoSite">
+            </a>
+            <h1>StarLove</h1>
+        </header>
+
+        <main> 
+        <div id="menu">
+            <ul>
+                <li><a href="index.html"> Accueil </a></li>
+                <li><a href="messagerie.php"> Messagerie </a></li>
+                <li><a href="recherche.php"> Recherche</a></li>
+            </ul>
         </div>
+            <content>
+            
+            </content>
+        </main>
+
+        <footer>
+        <div class="footer-nav">
+                <ul>
+                    <li><a href="#">Mentions légales</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+        </div>
+        <div class="copyright">
+            <p>&copy; 2024 Site de Rencontre - Tous droits réservés</p>
+        </div>
+    </footer>
     </body>
 </html>
+
