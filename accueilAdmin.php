6456dad5cae92818ff6201809076c088e750c8d6
Llasera <llaseratho@cy-tech.fr>
2024-03-29 15:27:09 +0100

    accueil admin

--- accueilAdmin.php
+++ accueilAdmin.php
@@ -29,12 +29,49 @@
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Accueil</title>
-    </head>
-    <body>
-        <div class="titre">
-          <h1 id="accueil">Accueil administrateur</h1>
+<head>
+    <title>Accueil</title>
+    <link rel="stylesheet" href="css/accueilAdmin.css">
+</head>
+<body>
+<header>
+    <div>
+        <a href="accueilUtilisateur.php">
+            <img src="css/img/logo.png" alt="Logo du site" id="LogoSite">
+        </a>
+        <h1>StarLove</h1>
+    </div>
+    <!-- Onglets à droite -->
+    <div id="onglets">
+        <ul>
+            <li><a href="#">Messages</a></li>
+            <li><a href="#">Consultations de profil</a></li>
+            <li><a href="#">Mon profil</a></li>
+            <li><a href="#">Qui a vu mon profil</a></li>
+        </ul>
+    </div>
+</header>
+<!-- Suggestion de profil -->
+<main>
+    <div class="profile-container">
+        <h2 id="accueil">Suggestion Profil</h2>
+        
+        <div class="bouton">
+            <button class="contact-button">Contacter</button>
+            <button class="suivant-button">Suivant</button>
         </div>
-    </body>
+    </div>
+</main>
+<footer>
+    <div class="footer-nav">
+        <ul>
+            <li><a href="#">Mentions légales</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+    <div class="copyright">
+        <p>&copy; 2024 Site de Rencontre - Tous droits réservés</p>
+    </div>
+</footer>
+</body>
 </html>
